ec435e960d65a2773bfca1d2f2a84df5feac88bf
Alexis <batista29alexis@gmail.com>
2026-05-09 20:28:22 -0500

    feat: add grid resize button with user prompt

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ function createGrid(size) {
 }
 
 createGrid(16);
-
 const squares = document.querySelectorAll(".square");
 squares.forEach(square => {
     square.addEventListener('mouseenter', () => {
@@ -29,3 +28,8 @@ const btn = document.createElement("button");
 btn.textContent = "Change grid size";
 btn.classList.add("btn");
 document.body.insertBefore(btn, container);
+
+btn.addEventListener('click', () => {
+    const userSquareSize = prompt("Enter the number of squares per side for the new grid");
+    createGrid(userSquareSize);
+})
